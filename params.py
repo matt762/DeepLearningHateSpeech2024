98c8b982f298366851987fdf73bb49f3e1e26a64
matt762 <matteo.fiore@net-c.com>
2024-04-21 17:53:23 +0200

    comment in param

--- params.py
+++ params.py
@@ -1,5 +1,5 @@
 #model = "google/bert_uncased_L-8_H-512_A-8"
-model = "google/bert_uncased_L-2_H-128_A-2"
-model_to_load = "./model_violence_L-4_H-256_A-4"
+model = "google/bert_uncased_L-2_H-128_A-2" # Pretrained model
+model_to_load = "./model_violence_L-4_H-256_A-4" # Model to load for inference
 feature = "violence"
 num_classes = 5
